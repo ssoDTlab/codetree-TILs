9962cf36f1ffe578a85b2f799ca68291d1ef8619
ssoDTlab <156610340+ssoDTlab@users.noreply.github.com>
2024-09-21 14:30:12 +0900

    Title: 더하기 사이클, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240921/더하기 사이클/additional-cycles.cpp
+++ 240921/더하기 사이클/additional-cycles.cpp
@@ -1,32 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sum(int n)
-{
-    int ret = 0;
-    while(n)
-    {
-        //143
-        ret += n % 10;
-        n /= 10;
-    }
-    return ret;
-
-}
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n; cin >> n;
+    
+    int n,t,s,nn;
+    cin >> n;
+    t = n;
 
-    int t = n;
     int cnt = 0;
     while(true)
     {
         ++cnt;
-        int ret = sum(t);
-        int new_number = t % 10 * 10 + ret % 10;
-        if(n == new_number) break;
-        t = new_number;
+        s = t % 10 + t / 10;
+        nn = t % 10 * 10 + s % 10;
+        if(n == nn) break;
+        t = nn;
     }
+
     cout << cnt;
     return 0;
 }
