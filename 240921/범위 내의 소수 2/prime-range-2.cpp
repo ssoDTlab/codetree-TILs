80596be03e5dbf33dafc0189799db1bd5a6ef031
ssoDTlab <156610340+ssoDTlab@users.noreply.github.com>
2024-09-21 14:42:32 +0900

    Title: 범위 내의 소수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240921/범위 내의 소수 2/prime-range-2.cpp
+++ 240921/범위 내의 소수 2/prime-range-2.cpp
@@ -5,26 +5,20 @@ vector<bool> isPrime(1000004,true);
 int main() {
     // 여기에 코드를 작성해주세요.
 
-    int a,b;
+    int a,b; 
     cin >> a >> b;
 
-    
-    for(int i = 2; i <= 1000000; ++i)
-    {
-        if(isPrime[i])
-        {
-            for(int j = i + i; j <= 1000000; j += i)
-                isPrime[j] = false;
-        }
-    }
-
+    isPrime[1] = false;
     int answer = 0;
-    for(int i = a; i <= b; ++i)
+    for(int i = a; i * i <= b; ++i)
     {
-        if(i == 1 || !isPrime[i]) continue;
-        ++answer;
+        if(!isPrime[i]) continue;
+        for(int j = i * i; j <= b; j += i)
+            isPrime[j] = false;
     }
 
+    for(int i = a; i <= b; ++i)
+        if(isPrime[i]) ++answer;
     cout << answer;
     return 0;
 }
