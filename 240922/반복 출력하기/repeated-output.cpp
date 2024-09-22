03eb00084d9f6a8279ccba12d751800dd9986622
ssoDTlab <156610340+ssoDTlab@users.noreply.github.com>
2024-09-22 21:16:19 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240922/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void Print(int n)
+{
+    for(int i = 0; i < n; ++i)
+        cout << "12345^&*()_\n";
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n; cin >> n;
+
+    Print(n);
+    return 0;
+}
