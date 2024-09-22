0305c33e7a16a9f9b91c7f3c76b3efc7acf18766
ssoDTlab <156610340+ssoDTlab@users.noreply.github.com>
2024-09-22 22:03:34 +0900

    Title: 최소공배수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240922/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findGCD(int n,int m)
+{
+    int gcd = 0;
+    for(int i = 1; i <= min(n,m); ++i)
+    {
+        if(n % i == 0 && m % i == 0)
+            gcd = i;
+    }
+    return gcd;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m;
+    cin >> n >> m;
+    
+    int gcd = findGCD(n,m);
+
+    cout << gcd * (n / gcd) * (m / gcd);
+    return 0;
+}
