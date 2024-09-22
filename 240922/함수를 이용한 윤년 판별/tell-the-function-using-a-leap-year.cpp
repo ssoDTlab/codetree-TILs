93aab10b05873e249989986df609cb1a7b882e36
ssoDTlab <156610340+ssoDTlab@users.noreply.github.com>
2024-09-22 22:45:40 +0900

    Title: 함수를 이용한 윤년 판별, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240922/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 240922/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 bool isyun(int year)
 {
-    return !(year % 100 == 0 && year % 400 != 0) || year % 4 == 0;
+    return !(year % 100 == 0 && year % 400 != 0) && year % 4 == 0;
 }
 int main() {
     // 여기에 코드를 작성해주세요.
