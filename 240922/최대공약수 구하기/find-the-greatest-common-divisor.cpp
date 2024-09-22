528e0ac5d43e206b47f6c4d0fcc1ed0724ee46dc
ssoDTlab <156610340+ssoDTlab@users.noreply.github.com>
2024-09-22 21:43:37 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240922/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 240922/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int gcd(int n,int m)
 {
-    int ret = 1;
-    for(int i = 2; i <= min(n,m); ++i)
+    int ret = 0;
+    for(int i = 1; i <= min(n,m); ++i)
     {
         if(n % i == 0 && m % i == 0)
-            ret *= i;
+            ret = i;
     }
     return ret;
 }
