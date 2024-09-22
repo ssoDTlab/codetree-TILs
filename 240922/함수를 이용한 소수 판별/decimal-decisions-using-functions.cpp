80712d2363cea9635711700906a2472721492ec9
ssoDTlab <156610340+ssoDTlab@users.noreply.github.com>
2024-09-22 22:38:45 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240922/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 240922/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool isPrime(int n)
 {
-    for(int i = 2; i <= n - 1; ++i)
+    for(int i = 2; i < n; ++i)
     {
         if(n % i == 0) return false;
     }
@@ -14,7 +14,7 @@ int main() {
     int a,b;
     cin >> a >> b;
     int ans = 0;
-    for(int i = a; i <= b; ++i)
+    for(int i = max(2,a); i <= b; ++i)
     {
         if(isPrime(i)) ans += i;
     }
